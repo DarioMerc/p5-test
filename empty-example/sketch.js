c45b41ed6dd21b3ab204b86f1fc078df3d0cfa2d
Dario Mercuri <dario.mercuri31@gmail.com>
2022-02-17 23:52:31 -0500

    test2

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -6,15 +6,15 @@ function setup() {
 function draw() {
   background(0)
 
-  rotateX(60)
+  rotateX(0) //set to 0 for spiral
 
   noFill()
   stroke(255)
 
   for (let i = 0; i < 50; i++) {
-    let r = map(sin(frameCount/3),-1,1,0,255)
-    let g = map(i,0,20,0,255)
-    let b = map(cos(frameCount/3),-1,1,255,0)
+    let r = map(sin(frameCount/3),-1,1,0,200)
+    let g = map(i,0,20,0,200)
+    let b = map(cos(frameCount/3),-1,1,200,0)
 
     stroke(r,g,b)
     rotate(frameCount/100 * i/7)
@@ -22,7 +22,7 @@ function draw() {
     for (let j = 0; j < 360; j += 45) {
       let rad = i * 3
       let x = rad * cos(j)
-      let y = rad * sin(j)
+      let y = rad * sin(j/3) // divide j by 3 for spiral
       let z = sin(frameCount * 2 + i * 5) * 50
       vertex(x, y, z)
     }
